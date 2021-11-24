0f92aefe626743515bbdf0bb668480cfaf5aae63
Alex <alexisguttlein@gmail.com>
2021-11-24 10:28:20 -0300

    Se crea clase Jugador con sus respectivos tests. Se crea clase Rango. Se agrega diagrama de clase general del juego. Se agrega diagrama de secuencia jugadorVisitaEdificio

---
+++ src/main/java/carmenSandiego/modelo/Jugador.java
@@ -0,0 +1,22 @@
+package carmenSandiego.modelo;
+
+import java.util.*;
+
+public class Jugador {
+    private String nombre;
+    private Rango rango;
+    //private Ciudad ciudadActual;
+
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.rango = new Rango();
+    }
+
+    public String getNombre(){return this.nombre;}
+
+    public String getRango(){return this.rango.getRango();}
+
+    public int getCantidadDeArrestos(){return this.rango.getCantidadDeArrestos();}
+
+    public void addArresto(){this.rango.addArresto();}
+}
