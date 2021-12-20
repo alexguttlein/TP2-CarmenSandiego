7cf826d5217fd054cd6b801950edf8b38eb890de
FabioSicca <fsicca@fi.uba.ar>
2021-12-20 16:28:22 -0300

    Primera prueba Interfaz grafica

---
+++ src/main/java/carmenSandiego/modelo/CaracteristicaCiudad.java
@@ -0,0 +1,13 @@
+package carmenSandiego.modelo;
+
+public class CaracteristicaCiudad {
+    private String caracteristica;
+
+    public CaracteristicaCiudad(String nuevaCaracteristica){
+        this.caracteristica = nuevaCaracteristica;
+    }
+
+    public String getCaracteristica(){
+        return caracteristica;
+    }
+}
