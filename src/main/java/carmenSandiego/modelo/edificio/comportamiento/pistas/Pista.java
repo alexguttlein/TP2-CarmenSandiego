1149af381fefe8bb975cf21d66cbd9b74402195a
FabioSicca <fsicca@fi.uba.ar>
2021-12-23 19:02:20 -0300

    Intento de integracion

---
+++ src/main/java/carmenSandiego/modelo/edificio/comportamiento/pistas/Pista.java
@@ -0,0 +1,16 @@
+package carmenSandiego.modelo.edificio.comportamiento.pistas;
+
+import carmenSandiego.modelo.Caracteristica;
+
+public interface Pista {
+
+    void setDialogo(Caracteristica dialogo);
+
+    void setDescripcion(Caracteristica descripcion);
+
+    Caracteristica getDialogo();
+
+    Caracteristica getDescripcion();
+
+    boolean compararPista(Pista pista);
+}
