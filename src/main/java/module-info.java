3b8156934db9e6334925ceaa22ece54f441e4e6e
Nichen710 <nichen@fi.uba.ar>
2021-12-27 20:57:53 -0300

    Update: diagramas de clase

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,11 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+
+    opens edu.fiuba.algo3 to javafx.fxml, javafx.graphics, javafx.base, javafx.controls;
+    opens edu.fiuba.algo3.controlador to javafx.fxml, javafx.controls, javafx.base, javafx.graphics;
     exports edu.fiuba.algo3;
+    exports edu.fiuba.algo3.controlador;
 }
