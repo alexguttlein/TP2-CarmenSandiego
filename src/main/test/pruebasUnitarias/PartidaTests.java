75193ca6965da526cea811813f2fe0bb32502b96
FabioSicca <fsicca@fi.uba.ar>
2021-12-27 01:19:40 -0300

    Modifico la prueba de PartidaTest

--- src/main/test/pruebasUnitarias/PartidaTests.java
+++ src/main/test/pruebasUnitarias/PartidaTests.java
@@ -17,16 +17,20 @@ public class PartidaTests {
     Ladron ladronMock = mock(Ladron.class);
     Interpol interpolMock = mock(Interpol.class);
     Ciudades ciudades = new Ciudades("src/main/java/edu/fiuba/algo3/datosDelJuego/ciudades.csv");
-    Partida partida = new Partida(jugadorMock, objetoRobadoMock, ladronMock, interpolMock, tiempoMock, ciudades);
+
 
     @Test
     public void partidaRecibeLosObjetosInstanciadosCorrectamente(){
 
         when(tiempoMock.getHoraActual()).thenReturn(7);
         when(jugadorMock.getCantidadDeArrestos()).thenReturn(0);
+        when(objetoRobadoMock.getImportancia()).thenReturn(new Caracteristica("Comun"));
+        when(objetoRobadoMock.getCiudadOrigen()).thenReturn(ciudades.getCiudades().get(4));
         when(objetoRobadoMock.getNombre()).thenReturn(new Caracteristica("Tesoro"));
         when(ladronMock.getGenero()).thenReturn(new Caracteristica("Femenino"));
         when(interpolMock.atraparSospechoso()).thenReturn(false);
+
+        Partida partida = new Partida(jugadorMock, objetoRobadoMock, ladronMock, interpolMock, tiempoMock, ciudades);
 
         assertEquals(tiempoMock.getHoraActual(), partida.getTiempo().getHoraActual());
         assertEquals(jugadorMock.getCantidadDeArrestos(), partida.getJugador().getCantidadDeArrestos());
